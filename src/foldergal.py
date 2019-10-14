3167fdf49007f145e271f0942b085a7fb1e55259
Velichko Karakostov <vivo@specto.org>
2019-10-14 17:56:10 +0300

    A primitive recursive browser is available!

--- src/foldergal.py
+++ src/foldergal.py
@@ -1,22 +1,64 @@
 import asyncio
+from sanic.exceptions import ServerError
+from sanic.log import logger
+from pathlib import Path
 
 CONFIG = {}
-
+files = {}
 
 def configure(config):
     global CONFIG
     CONFIG['FOLDER_ROOT'] = config['FOLDER_ROOT']
     CONFIG['RESCAN_SECONDS'] = config['RESCAN_SECONDS']
+    CONFIG['TARGET_EXT'] = config['TARGET_EXT']
+
+def normalize_path(p):
+    return './' + p.relative_to(CONFIG['FOLDER_ROOT']).as_posix()
+
+
+async def scan(folder):
+    contents = {}
+    root = Path(folder)
+    if not root.exists():
+        logger.error(f'Folder not found "{root}"')
+        return contents
+
+    for child in root.iterdir():
+        if child.is_dir():
+            sub_contents = await scan(child)
+            if sub_contents:
+                contents[normalize_path(child)] = sub_contents
+        elif child.suffix.lower() in CONFIG['TARGET_EXT']:
+            contents[normalize_path(child)] = child.name
+    return contents
+
 
 
 async def refresh():
+    global files
     if not CONFIG:
-        raise UnboundLocalError("Call foldergal.configure")
+        raise ServerError("Call foldergal.configure")
     while True:
-        print('Refreshing')
+        logger.debug('Refreshing...')
+        files = await scan(CONFIG['FOLDER_ROOT'])
         await asyncio.sleep(CONFIG['RESCAN_SECONDS'])
 
 
 async def get_file_by_id(id):
     if not CONFIG:
-        raise UnboundLocalError("Call foldergal.configure")
+        raise ServerError("Call foldergal.configure")
+
+
+async def get_folder_contents(target=None, sub_items = None):
+    if target == './':
+        return 'folder', files.keys()
+    current_folder = sub_items if sub_items else files
+    for name, item in current_folder.items():
+        if name == target:
+            if isinstance(item, dict):
+                return 'folder', item
+            return 'image', name
+        elif isinstance(item, dict):
+            sub_item = await get_folder_contents(target, item)
+            if sub_item:
+                return sub_item
--- src/www.py
+++ src/www.py
@@ -3,11 +3,12 @@ import os
 import foldergal
 from sanic import Sanic, response
 from sanic.log import logger
+from sanic.exceptions import NotFound
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 # Initialize framework for our app and load config from file
-app = Sanic(__name__)
+app = Sanic(__name__, strict_slashes=False)
 app.config.from_pyfile(
     os.path.join(os.path.dirname(__file__),
                  "../foldergal.cfg"))
@@ -32,25 +33,55 @@ def render(template, **kwargs):
     return template.render(url_for=app.url_for, **kwargs)
 
 
-@app.route("/")
-async def index(req):
-    return response.html(render('list.html', message="Welcome"))
+@app.route('/test')
+async def view_slash(req):
+    return response.html(render('view.html', body="test wazaaaaa"))
 
-@app.route("/view/<file>")
-async def index(req, file):
-    return response.html(render('view.html', file=file))
 
 @app.route("/get/<file>")
-async def index(req, file):
+async def get(req, file):
     file_data = await foldergal.get_file_by_id(file)
-    if not file_data:
+    if file_data:
+        return await response.file_stream(file_data)
+    return response.html(
+        render('error.html', message=f'File "{file}" was not found'),
+        status=404
+    )
+
+# This must be the last route
+@app.route("/")
+@app.route("/<path:path>")
+async def index(req, path=''):
+    path = './' + path
+    logger.debug(path)
+    try:
+        ftype, content = (await foldergal.get_folder_contents(path))
+    except Exception as e:
+        logger.debug(e)
         return response.html(
-            render('error.html', message=f'File "{file}" was not found'),
+            render('error.html', message=f'"{path}" was not found'),
             status=404
         )
-    else:
-        return await response.file_stream(file_data)
+    if ftype == 'image':
+        # this is path to file
+        return await response.file_stream(app.config['FOLDER_ROOT'] + content)
+    return response.html(render('list.html', folders=[c[1:] for c in content]))
 
+
+# Display some error message when things break
+async def server_error_handler(request, exception):
+    if isinstance(exception, NotFound):
+        return response.html(render('error.html', title="Not found",
+                                    message=exception), status=404)
+    # It might be serious
+    logger.error(exception)
+    return response.html(
+        render('error.html',
+               heading="An error has occurred",
+               message="Check the logs for clues how to fix it."),
+        status=500
+    )
+app.error_handler.add(Exception, server_error_handler)
 
 if __name__ == "__main__":
     logger.info(f'Starting server v{app.config["VERSION"]}')
@@ -60,4 +91,5 @@ if __name__ == "__main__":
         debug=app.config["DEBUG"],
         access_log=app.config["DEBUG"],
         workers=app.config["WORKERS"],
+        auto_reload=False,
     )
