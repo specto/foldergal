2068e6a912709be838e29969bcf6eebaf4aac60b
Velichko Karakostov <vivo@specto.org>
2020-04-21 16:34:02 +0300

    Implement filter by media type

--- src/foldergal.go
+++ src/foldergal.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/gabriel-vasile/mimetype"
+	"mime"
 	"net/http"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
@@ -12,7 +13,6 @@ func containsDotFile(name string) bool {
 	parts := strings.Split(name, "/")
 	for _, part := range parts {
 		if strings.HasPrefix(part, ".") {
-			logger.Printf("Detected dot: %s", name)
 			return true
 		}
 	}
@@ -21,10 +21,23 @@ func containsDotFile(name string) bool {
 
 var mimePrefixes = regexp.MustCompile("^(image|video|audio|application/pdf)")
 
-func validMediaFile(name string) bool {
-	detectedMime, _ := mimetype.DetectFile(name)
-	logger.Printf("Detected mime %s for: %s", name, detectedMime.String())
-	match := mimePrefixes.FindStringSubmatch(detectedMime.String())
+func validMediaByExtension(name string) bool {
+	ext := filepath.Ext(name)
+	contentType := mime.TypeByExtension(ext)
+	match := mimePrefixes.FindStringSubmatch(contentType)
+	return match != nil
+}
+
+func validMediaFile(file http.File, name string) bool {
+	buffer := make([]byte, 512)
+	_, err := file.Read(buffer)
+	// Reset seek to start to be able to read the file later
+	_, _ = file.Seek(0, 0)
+	if err != nil {
+		return false
+	}
+	contentType := http.DetectContentType(buffer)
+	match := mimePrefixes.FindStringSubmatch(contentType)
 	return match != nil
 }
 
@@ -45,13 +58,20 @@ func (fs filteredFileSystem) Open(name string) (http.File, error) {
 	if err != nil {
 		return nil, err
 	}
-	return filteredFile{file}, err
+	stat, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if !stat.IsDir() && !validMediaFile(file, name) {
+		return nil, os.ErrNotExist
+	}
+	return filteredFile{file}, nil
 }
 
 func (f filteredFile) Readdir(n int) (fis []os.FileInfo, err error) {
 	files, err := f.File.Readdir(n)
-	for _, file := range files { // Filter out the dot files from listing
-		if !containsDotFile(file.Name()) {
+	for _, file := range files { // Filter out the dot and non-media files from listing
+		if !containsDotFile(file.Name()) && (file.IsDir() || validMediaByExtension(file.Name())) {
 			fis = append(fis, file)
 		}
 	}
