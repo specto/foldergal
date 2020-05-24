f1331bfc77de75ac14140b2b50555b09c231e245
Velichko Karakostov <vivo@specto.org>
2020-05-24 16:05:22 +0300

    Show video and audio
    Simulate history with pushState.

--- res/script.js
+++ res/script.js
@@ -1,25 +1,90 @@
 let currentlyShown;
+let originalHref;
 
-function showSlide(elem) {
-    currentlyShown = elem;
+function showMedia(href, mediaClass) {
     const modal = document.getElementById("slideshow");
-    modal.innerHTML = `<img src="${elem.href}" />`
+    if (mediaClass === "video") {
+        modal.innerHTML = `<video controls="true" autoplay="true">
+            <source src="${href}" /></video>`;
+    } else if (mediaClass === "audio") {
+        modal.innerHTML = `<audio controls="true" autoplay="true">
+            <source src="${href}" /></audio>`;
+    } else {
+        modal.innerHTML = `<img src="${href}" />`;
+    }
     modal.style.display = "block";
+}
+
+function showSlide(elem, mediaClass) {
+    if (!currentlyShown) {
+        originalHref = document.location.href;
+    }
+    currentlyShown = elem;
+    const href = elem.href;
+    showMedia(href, mediaClass);
+    elem.focus();
+    history.pushState({"url": href, "className": mediaClass}, href, href);
     return false;
+}
+
+function historyHandle(ev) {
+    if (ev.state) {
+        showMedia(ev.state.url, ev.state.className);
+        document.getElementById("slideshow").style.display = "block";
+    } else {
+        document.getElementById("slideshow").style.display = "none";
+        document.getElementById("slideshow").innerHTML = "";
+    }
+}
+
+function findPrev(current) {
+    if (!current) {
+        return false;
+    }
+    const prevLi = current.parentNode.previousElementSibling;
+    if (!prevLi) {
+        return false;
+    } else if (prevLi.className === 'folder' || prevLi.className === "") {
+        return findPrev(prevLi.querySelector("a"));
+    }
+    return prevLi.querySelector("a");
 }
 
 function prevSlide() {
     if (!currentlyShown) {
         return;
     }
-    console.log(currentlyShown);
+    const prev = findPrev(currentlyShown);
+    if (prev) {
+        prev.click();
+    } else {
+        cancelSlide();
+    }
+}
+
+function findNext(current) {
+    if (!current) {
+        return false;
+    }
+    const nextLi = current.parentNode.nextElementSibling;
+    if (!nextLi) {
+        return false;
+    } else if (nextLi.className === 'folder' || nextLi.className === "") {
+        return findNext(nextLi.querySelector("a"));
+    }
+    return nextLi.querySelector("a");
 }
 
 function nextSlide() {
     if (!currentlyShown) {
         return;
     }
-    console.log(currentlyShown);
+    const next = findNext(currentlyShown);
+    if (next) {
+        next.click();
+    } else {
+        cancelSlide();
+    }
 }
 
 function cancelSlide() {
@@ -27,13 +92,21 @@ function cancelSlide() {
     modal.style.display = "none";
     modal.innerHTML = "";
     currentlyShown = null;
-    return false;
+    history.pushState(null, originalHref, originalHref);
+}
+
+function clickHandle(ev) {
+    if (ev && ["IMG", "VIDEO", "AUDIO"].includes(ev.target.tagName)) {
+        return false;
+    }
+    cancelSlide();
 }
 
 function keyHandle(ev) {
     if (!currentlyShown) {
         return;
     }
+
     switch (ev.code) {
     case "Backspace":
     case "Delete":
@@ -58,8 +131,8 @@ function keyHandle(ev) {
 }
 
 function init() {
-    console.log ("initializing")
     window.addEventListener("keydown", keyHandle);
-    document.getElementById("slideshow").addEventListener("click", cancelSlide)
+    window.addEventListener("popstate", historyHandle);
+    document.getElementById("slideshow").addEventListener("click", clickHandle);
 }
 window.addEventListener("load", init)
